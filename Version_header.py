b8ce5175ddecaa49726842618bc9255a217f139e
Mehmet Alperen Bakıcı <mehmetalperen97@hotmail.com>
2023-06-03 00:04:59 +0300

    v0.0.1 First Upload
    """
    -------------------------------------------------------------------------------------
                                  ISTANBUL TECHNICAL UNIVERSITY
                                              MARIN NL
    
                                            RESTRICTED
    -------------------------------------------------------------------------------------
    
    Version      : v0.0.1
    Author       : Mehmet Alperen BAKICI
    Release Date : 03.06.2023
    
    -------------------------------------------------------------------------------------
    
    Notes:
        This is the first upload for the Autonomous Undervehicle Project on GitHub Repo
    
    """

---
+++ Version_header.py
@@ -0,0 +1,19 @@
+"""
+-------------------------------------------------------------------------------------
+                              ISTANBUL TECHNICAL UNIVERSITY
+                                          MARIN NL
+
+                                        RESTRICTED
+-------------------------------------------------------------------------------------
+
+Version      : v0.0.1
+Author       : Mehmet Alperen BAKICI
+Release Date : 03.06.2023
+
+-------------------------------------------------------------------------------------
+
+Notes: 
+    This is the first upload for the Autonomous Undervehicle Project on GitHub Repo
+
+"""
+   
