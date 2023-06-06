81a5c6d87ce54c48f3b97fd1986246764a462092
Mehmet Alperen <mehmetalperenb97@gmail.com>
2023-06-06 23:59:58 +0300

    v0.0.2.401
    -------------------------------------------------------------------------------------
    Version      : v0.0.2.401
    Author       : Mehmet Alperen BAKICI
    Release Date : 06.06.2023 23:50
    Branch Name  : Model_And_Hyperparameters_Changes
    -------------------------------------------------------------------------------------
    Notes:
        Adding new layer to the model.py
        Note: When adding a new layer this creates a good results than others.
    --------------------------------------------------------------------------------

--- Version_header.py
+++ Version_header.py
@@ -6,66 +6,68 @@
                                         RESTRICTED
 -------------------------------------------------------------------------------------
 
+-------------------------------------------------------------------------------------
+Version      : v0.0.2.401
+Author       : Mehmet Alperen BAKICI
+Release Date : 06.06.2023 23:50
+Branch Name  : Model_And_Hyperparameters_Changes
+-------------------------------------------------------------------------------------
+Notes:
+    Adding new layer to the model.py
+    Note: When adding a new layer this creates a good results than others.
+--------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------------
+Version      : v0.0.2.400
+Author       : Mehmet Alperen BAKICI
+Release Date : 03.06.2023 23:5
+Branch Name  : Model_And_Hyperparameters_Changes
+-------------------------------------------------------------------------------------
+Notes:
+    Creating new branch to work every python file.
+    State_size chganged 11 to 9. because Our lecturer wants to 9 state_Size for the
+    project purpose
+--------------------------------------------------------------------------------
 
+-------------------------------------------------------------------------------------
 Version      : v0.0.2
 Author       : Mehmet Alperen BAKICI
 Release Date : 03.06.2023 01:10
-Branch Name  : Main 
-
+Branch Name  : Main
 -------------------------------------------------------------------------------------
-
 Notes: 
     Merge 'DDPG Integration' branch into 'Main' branch
-
 --------------------------------------------------------------------------------
 
-
 -------------------------------------------------------------------------------------
-
 Version      : v0.0.1.201
 Author       : Mehmet Alperen BAKICI
 Release Date : 03.06.2023
 Branch Name  : DDPG_Entegrasyonu
-
 -------------------------------------------------------------------------------------
-
 Notes: 
     Yasin's working files was added. These files updated new versions.
     * DDPG.ipynb
     * VerySimpleAuv.py
-
 -------------------------------------------------------------------------------------
 
-
 -------------------------------------------------------------------------------------
-
 Version      : v0.0.1.200
 Author       : Mehmet Alperen BAKICI
 Release Date : 03.06.2023
 Branch Name  : DDPG_Entegrasyonu
-
 -------------------------------------------------------------------------------------
-
 Notes: 
     This is DDPG integration branch and its first upload commit.
-
 -------------------------------------------------------------------------------------
 
-
-
 -------------------------------------------------------------------------------------
-
 Version      : v0.0.1
 Author       : Mehmet Alperen BAKICI
 Release Date : 03.06.2023
-
 -------------------------------------------------------------------------------------
-
 Notes: 
     This is the first upload for the Autonomous Undervehicle Project on GitHub Repo
+-------------------------------------------------------------------------------------
+"""
 
-
-
-   
