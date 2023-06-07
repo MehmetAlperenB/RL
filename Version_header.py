209bdfad969f4215255b1206a56f928db8359954
Mehmet Alperen <mehmetalperenb97@gmail.com>
2023-06-07 03:24:30 +0300

    v0.0.2.405
    -------------------------------------------------------------------------------------
    Version      : v0.0.2.405
    Author       : Mehmet Alperen BAKICI, Siyami Gurkan KUZUCU
    Release Date : 07.06.2023 03:00
    Branch Name  : Model_And_Hyperparameters_Changes
    -------------------------------------------------------------------------------------
    Notes:
        Changing hyperparameters. Learning rates are changed:
        OLD --> NEW :
        BUFFER_SIZE  1e5 --> 1e6  # replay buffer size
        BATCH_SIZE   128 --> 256  # minibatch size
    
        Hyperparameters
        BUFFER_SIZE = int(1e6)  # replay buffer size
        BATCH_SIZE = 256        # minibatch size
        GAMMA = 0.99            # discount factor
        TAU = 1e-3              # for soft update of target parameters
        LR_ACTOR = 1e-5         # learning rate of the actor
        LR_CRITIC = 1e-4        # learning rate of the critic
        WEIGHT_DECAY = 0        # L2 weight decay
    -------------------------------------------------------------------------------------

--- Version_header.py
+++ Version_header.py
@@ -6,6 +6,28 @@
                                         RESTRICTED
 -------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------
+Version      : v0.0.2.405
+Author       : Mehmet Alperen BAKICI, Siyami Gurkan KUZUCU
+Release Date : 07.06.2023 03:00
+Branch Name  : Model_And_Hyperparameters_Changes
+-------------------------------------------------------------------------------------
+Notes:
+    Changing hyperparameters. Learning rates are changed:
+    OLD --> NEW :
+    BUFFER_SIZE  1e5 --> 1e6  # replay buffer size
+    BATCH_SIZE   128 --> 256  # minibatch size
+
+    Hyperparameters
+    BUFFER_SIZE = int(1e6)  # replay buffer size
+    BATCH_SIZE = 256        # minibatch size
+    GAMMA = 0.99            # discount factor
+    TAU = 1e-3              # for soft update of target parameters
+    LR_ACTOR = 1e-5         # learning rate of the actor
+    LR_CRITIC = 1e-4        # learning rate of the critic
+    WEIGHT_DECAY = 0        # L2 weight decay
+-------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------
 Version      : v0.0.2.404
 Author       : Mehmet Alperen BAKICI, Mustafa UNVER, Yasin ERTAS, Siyami Gurkan KUZUCU,
 Release Date : 07.06.2023 02:45
